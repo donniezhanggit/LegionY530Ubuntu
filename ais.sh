fe09d825b19eb045e0c345faec6dc473689f25d3
crazieNephilim <37982778+crazieNephilim@users.noreply.github.com>
2019-06-11 10:55:58 +0200

    Update ais.sh

--- ais.sh
+++ ais.sh
@@ -16,6 +16,22 @@ echo "*** UPDATE and UPGRADE *** START ***"
 sudo apt-get update && sudo apt-get upgrade
 echo "*** UPDATE and UPGRADE *** END ***"
 
+# Nvidia drivers
+# Remove old drivers
+sudo apt-get purge nvidia*
+# Get Repo
+sudo add-apt-repository ppa:graphics-drivers -y
+sudo apt-get update
+# Safeguard SSH
+sudo apt-get install screen
+screen
+# List drivers
+ubuntu-drivers list
+# Install driver
+sudo apt-get install nvidia-driver-390
+# Install Check
+nvidia-smi
+
 # unetbootin
 echo "*** unetbootin *** START ***"
 sudo add-apt-repository ppa:gezakovacs/ppa -y
